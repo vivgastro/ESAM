7c00145d3b3f8b39c1c6073f3b9899090e5fbd4d
Vivek Gupta <vivg269@gmail.com>
2024-09-20 18:55:32 +1000

    Added option to specify label in plot_fdmt_recovery.py

--- FDMT_tests/plot_fdmt_recovery.py
+++ FDMT_tests/plot_fdmt_recovery.py
@@ -6,9 +6,15 @@ import pandas as pd
 def main():
     f = plt.figure()
     ax = f.add_subplot(111)
-    for fname in args.fname:
+    for ii, fname in enumerate(args.fname):
+        if len(args.labels) > 0:
+            label = args.labels[ii]
+        else:
+            label = fname
         results = pd.read_csv(fname, sep='\s+')
-        ax.plot(results['DM'], results['BZ_FDMT_snr'] / results['Max_snr'], label=fname)
+        ax.plot(results['DM'], results['BZ_FDMT_snr'] / results['MF_snr'], label=label)
+    
+    plt.axhline(1, ls = '--', c='k')
     plt.legend()
     plt.xlabel("DM [samples]")
     plt.ylabel("Fraction of S/N recovered")
@@ -17,5 +23,6 @@ def main():
 if __name__ == '__main__':
     a = argparse.ArgumentParser()
     a.add_argument("fname", type=str, nargs = '+',  help="Path to the FDMT performance results file")
+    a.add_argument("-labels", type=str, nargs = '+', help="Labels")
     args = a.parse_args()
     main()
